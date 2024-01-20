fa784066d3614fff6fd11863dc4dbb0ca3257209
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-01-21 01:26:25 +0200

    This commit for spring boot rest api with path variables

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springbootrestapi/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springbootrestapi/demo/rest/StudentRestController.java
@@ -2,7 +2,9 @@ package com.springbootrestapi.demo.rest;
 
 
 import com.springbootrestapi.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +15,36 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    // define a field, load the field with data. do it only once
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Mohamed", "Fathy"));
+        theStudents.add(new Student("Ahmed", "Karim"));
+        theStudents.add(new Student("Ali", "yousef"));
+    }
+
     // define endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents(){
 
-        List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("Mohamed", "Fathy"));
-        theStudents.add(new Student("Ahmed", "Karim"));
-        theStudents.add(new Student("Ali", "yousef"));
-
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" ~ return at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // Just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 
 }
