ee58fea4c6d14b4f8283de77fdff09aa352a96ee
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-01-20 12:52:03 +0200

    This is the Spring Boot Rest POJO

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springbootrestapi/demo/rest/StudentRestController.java
@@ -0,0 +1,36 @@
+package com.springbootrestapi.demo.rest;
+
+
+import com.springbootrestapi.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Mohamed", "Fathy"));
+        theStudents.add(new Student("Ahmed", "Karim"));
+        theStudents.add(new Student("Ali", "yousef"));
+
+        return theStudents;
+    }
+
+}
+
+
+
+
+
+
+
