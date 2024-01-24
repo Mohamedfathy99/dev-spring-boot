5542c26b74fa3a91dc0b0b3ef96b5bd56917d60d
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-01-24 03:14:28 +0200

    This is spring boot Rest: Add, Update and delete

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,8 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,5 +45,43 @@ public class EmployeeRestController {
         }
         return theEmployee;
     }
+
+    // add mapping for Post /employees ~ add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // also just in case they pass an id in JSON .... set id to 0
+        // this is to force a save of new item ... instead of update
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+
+    }
+
+
+    // add mapping for delete /employees/ {employeeId} ~ delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String  deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if (tempEmployee == null){
+            throw new RuntimeException("This employee id didn't exist ~ " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id ~ " + employeeId;
+
+    }
+
+
 
 }
