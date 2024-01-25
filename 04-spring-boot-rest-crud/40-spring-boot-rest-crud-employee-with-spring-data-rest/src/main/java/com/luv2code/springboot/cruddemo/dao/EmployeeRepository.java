75d3690171901c4f48ebb324566dd31a17211c04
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-01-25 04:10:10 +0200

    This is Spring Boot REST: Spring Data REST Configs and Sorting

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// This annotation because Spring Boot Data Rest doesn't handle some plural words
+// so, we use this annotation for this reason
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
         // that's it .... no need to write any code LoooooL!!!
