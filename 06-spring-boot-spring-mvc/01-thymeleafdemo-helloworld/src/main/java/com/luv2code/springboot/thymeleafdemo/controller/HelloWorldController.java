4b192c821cf710dc3aa05cfe9a771b67cef5a967
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-02-03 05:29:59 +0200

    This is commit for Spring Boot - Adding Data to Spring MVC Model

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +19,27 @@ public class HelloWorldController {
     // need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    // need a controller method to read data from and
+    // add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add the message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
