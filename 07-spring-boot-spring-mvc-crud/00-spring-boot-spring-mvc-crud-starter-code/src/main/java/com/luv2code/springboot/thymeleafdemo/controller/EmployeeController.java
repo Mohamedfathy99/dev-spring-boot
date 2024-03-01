4a4a5ca11867828e3a8ef4128c94c72db8db80be
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-03-01 12:12:48 +0200

    Update EmployeeController.java - CRUD Database Project - Delete Employee

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -67,4 +67,15 @@ public class EmployeeController {
         return "redirect:/employees/list";
     }
 
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+        
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to the /employees/list
+        return "redirect:/employees/list";
+    }
+
 }
