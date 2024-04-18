0d0b58ec8f993ddb065cbe265f2921ded85e154a
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-04-19 01:57:54 +0200

    Create AppDAOImplementation .java to implements App.DAO.java

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImplementation.java
@@ -0,0 +1,29 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AppDAOImplementation implements AppDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+
+    @Autowired
+    public AppDAOImplementation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        // That's for save an instructor
+        entityManager.persist(instructor);
+    }
+
+
+
+}
