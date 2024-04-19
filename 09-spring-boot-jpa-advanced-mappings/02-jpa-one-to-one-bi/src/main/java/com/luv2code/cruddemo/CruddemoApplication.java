2796a0987cb468839206ba8c3bd755e6eadacc8a
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-04-19 16:14:20 +0200

    Update CruddemoApplication.java

--- 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -32,7 +32,7 @@ public class CruddemoApplication {
 
 	private void deleteInstructorDetail(AppDAO appDAO) {
 
-		int id = 2;
+		int id = 3;
 		System.out.println("Deleting instructor Detail id: " + id);
 
 		appDAO.deleteInstructorDetailById(id);
