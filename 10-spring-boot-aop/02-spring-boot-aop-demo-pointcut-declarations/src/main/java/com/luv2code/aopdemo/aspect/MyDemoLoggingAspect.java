6ff1fe031098eb43b3bf66424abc94000ca9cc43
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-05-05 16:06:15 +0300

    Update MyDemoLoggingAspect.java - AOP: @AfterThrowing Advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo.aspect;
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -17,8 +18,24 @@ import java.util.Locale;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-    // add a new advice for @AfterReturning on the findAccounts method
+    @AfterThrowing(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            throwing = "theExc"
+    )
+    public void afterThrowingFindAccountsAdvice(
+            JoinPoint theJoinPoint, Throwable theExc){
 
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Execution @AfterThrowing on method: " + method);
+
+        // log the exception
+        System.out.println("\n=====>>> The exception is: " + theExc);
+
+    }
+
+
+    // add a new advice for @AfterReturning on the findAccounts method
     @AfterReturning(
             pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
             returning = "result"
