a354dcf2b164f79b95b47d0c240fbddbd47adfd1
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-05-05 17:06:25 +0300

    Update AopdemoApplication.java - AOP: @After Advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -21,11 +21,37 @@ public class AopdemoApplication {
 		return runner -> {
 
 //			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+
 //			demoTheAfterReturningAdvice(theAccountDAO);
 
-			demoTheAfterThrowingAdvice(theAccountDAO);
+//			demoTheAfterThrowingAdvice(theAccountDAO);
+
+  			demoTheAfterAdvice(theAccountDAO);
 
 		};
+	}
+
+	private void demoTheAfterAdvice(AccountDAO theAccountDAO) {
+
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = false;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		}
+		catch (Exception exception){
+			System.out.println("\n\nMain Program: ... caught the exception: " + exception);
+		}
+
+		// display the accounts
+		System.out.println("\n\nMain Program: demoTheAfterThrowingAdvice");
+		System.out.println("-------");
+
+		System.out.println(theAccounts);
+
+		System.out.println("\n");
 	}
 
 	private void demoTheAfterThrowingAdvice(AccountDAO theAccountDAO) {
