334e35b192c69db517eb12dcced3d39242bb26ee
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-05-06 09:58:41 +0300

    AOP: Integrating AOP with Spring MVC CRUD App

--- 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -36,7 +37,31 @@ public class DemoLoggingAspect {
         // display method we are calling
         String theMethod = theJoinPoint.getSignature().toShortString();
         myLogger.info("=====>> in @Before: calling method: " + theMethod);
+
         // display the arguments to the method
 
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+        // loop through and display arguments
+        for (Object tempArg : args){
+            myLogger.info("=====>> argument: " + tempArg);
+        }
     }
+
+    // add @AfterReturning advice
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult){
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @AfterReturning: from method: " + theMethod);
+
+        // display data returned
+        myLogger.info("====>> result: " + theResult);
+    }
+
+
 }
